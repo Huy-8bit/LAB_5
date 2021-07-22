5142c2e30e481b629f49b1c1d73a50a3e55dd6fe
Huy-8bit <85545963+Huy-8bit@users.noreply.github.com>
2021-07-23 01:19:34 +0700

    xong

--- 20127185_BST.cpp
+++ 20127185_BST.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
-#include <fstream>
 #include <string>
 #include <sstream>
-#include <cstdlib>
-#include <ctime>
+#include <fstream>
 using namespace std;
 struct info
 {
     string keys;
     string values;
 };
+struct Node
+{
+    info data;
+    Node *left;
+    Node *right;
+};
+
+typedef Node *Tree;
+
 int sizeFileInput(fstream &fsInFile)
 {
     int size = 0;
@@ -21,9 +28,8 @@ int sizeFileInput(fstream &fsInFile)
     }
     return size;
 }
-void inputFile()
+void input(fstream &fsInFile)
 {
-    fstream fsInFile("Oxford English info.txt", ios::in);
     fstream fsTest("keys.txt", ios::in);
     int size = sizeFileInput(fsTest);
     fsTest.close();
@@ -42,7 +48,7 @@ void inputFile()
                 string values;
                 stringstream ss(line);
                 getline(ss, keys, ' ');
-                getline(ss, values, ' ');
+                getline(ss, values, '\n');
                 fs << keys << endl;
                 fs1 << values << endl;
             }
@@ -53,348 +59,291 @@ void inputFile()
         fs1.close();
     }
 }
-struct Node
-{
-    info Data;
-    Node *left;
-    Node *right;
-};
-typedef Node *Tree;
 
-Node *createNode(info D)
+Node *CreateNode(string keys, string value)
 {
     Node *p = new Node;
-    p->Data = D;
+    p->data.keys = keys;
+    p->data.values = value;
     p->left = NULL;
     p->right = NULL;
     return p;
 }
 
-void addNode(Tree &t, Node *node)
+void CreateTree(Tree &root)
 {
-    if (t != NULL)
+    root = NULL;
+}
+
+void DestroyTree(Tree &root)
+{
+    if (root)
     {
-        if (t->Data.keys == node->Data.keys)
+        DestroyTree(root->left);
+        DestroyTree(root->right);
+        delete root;
+    }
+}
+
+void AddNode(Tree &root, Node *node)
+{
+    if (root)
+    {
+        // if (root->data.keys == node->data.keys)
+        // {
+        //     return;
+        // }
+        if (node->data.keys <= root->data.keys)
         {
-            return;
-        }
-        if (t->Data.keys > node->Data.keys)
-        {
-            addNode(t->left, node);
+            AddNode(root->left, node);
         }
         else
         {
-            addNode(t->right, node);
+            AddNode(root->right, node);
         }
     }
     else
     {
-        t = node;
+        root = node;
     }
 }
 
-Node *minValueNode(Node *node)
+Node *FindNode(Tree root, string keys)
 {
-    Node *p = node;
-    while (p && p->left != NULL)
+    if (root)
     {
-        p = p->left;
-    }
-    return p;
-}
-Node *deleteNode(Node *node, string s)
-{
-    if (node == NULL)
-        return node;
-
-    if (s < node->Data.keys)
-        node->left = deleteNode(node->left, s);
-    else if (s > node->Data.keys)
-        node->right = deleteNode(node->right, s);
-    else
-    {
-        if (node->left == NULL && node->right == NULL)
+        if (root->data.keys == keys)
         {
-            delete node;
-            return NULL;
+            return root;
         }
-        else if (node->left == NULL)
+        if (keys < root->data.keys)
         {
-            Node *p = node->right;
-            delete node;
-            return p;
+            return FindNode(root->left, keys);
         }
-        else if (node->right == NULL)
-        {
-            Node *p = node->left;
-            delete node;
-            return p;
-        }
-
-        Node *p = minValueNode(node->right);
-        node->Data = p->Data;
-        node->right = deleteNode(p, node->Data.keys);
+        return FindNode(root->right, keys);
     }
-
-    return node;
+    return NULL;
 }
 
-void processingUsage(info *D, int &size)
+void saveDataTree(Tree root, fstream &fs1, fstream &fs2)
 {
-    for (int i = 0; i < size; ++i)
+    if (root == NULL)
     {
-        if (D[i].keys == "Usage")
-        {
-            if (D[i].values[0] != 'n' || D[i].values[1] != '.')
-            {
-                D[i - 1].values += " " + D[i].keys + ": " + D[i].values;
-                D[i].keys = D[size - 1].keys;
-                D[i].values = D[size - 1].values;
-                --size;
-            }
-        }
-    }
-}
-void delRepeat(info *D, int &size)
-{
-    for (int i = 0; i < size; ++i)
-    {
-        for (int j = i + 1; j < size; ++j)
-        {
-            if (D[j].values == D[i].values)
-            {
-                D[j].keys = D[size - 1].keys;
-                D[j].values = D[size - 1].values;
-                --size;
-            }
-        }
-    }
-}
-void load(Tree &t)
-{
-    fstream f;
-    string s;
-    int size(0);
-
-    f.open("Oxford English Dictionary.txt", ios::in);
-    while (!f.eof())
-    {
-        getline(f, s);
-        if (s.size() > 3)
-            ++size;
-    }
-    f.clear();
-    f.seekg(0L, ios::beg);
-
-    info *D = new info[size];
-    for (int i = 0; i < size; ++i)
-    {
-        do
-        {
-            getline(f, s);
-        } while (s.size() < 3);
-
-        stringstream ss(s);
-        string s1;
-
-        getline(ss, D[i].keys, ' ');
-        getline(ss, s1, ' ');
-        if (s1.size() != 0)
-        {
-            D[i].keys += " " + s1;
-            getline(ss, s1, ' ');
-            if (s1.size() != 0)
-            {
-                D[i].keys += " " + s1;
-                getline(ss, s1, ' ');
-            }
-        }
-        getline(ss, D[i].values, '\n');
-    }
-    f.close();
-    delRepeat(D, size);
-    processingUsage(D, size);
-
-    for (int i = 0; i < size; ++i)
-    {
-        int temp = rand() % size + 0;
-        swap(D[i], D[temp]);
-    }
-
-    for (int i = 0; i < size; ++i)
-        addNode(t, createNode(D[i]));
-
-    delete[] D;
-}
-
-Node *find(Tree t, string s)
-{
-    if (t)
-    {
-        if (t->Data.keys == s)
-            return t;
-        if (t->Data.keys > s)
-            return find(t->left, s);
-        else
-            return find(t->right, s);
-    }
-    else
-        return NULL;
-}
-void search(Tree t)
-{
-    Node *result;
-    string key;
-
-    cout << "Enter a word to translate meaning: ";
-    getline(cin, key);
-    if (key[0] >= 'a' && key[0] <= 'z')
-        key[0] -= 32;
-
-    result = find(t, key);
-    if (result == NULL)
-        cout << "Can't find the worData." << endl;
-    else
-        cout << result->Data.keys + ": " + result->Data.values + "" << endl;
-}
-
-void addNewWord(Tree &t)
-{
-    info Data;
-
-    cout << "Enter the word for adding: ";
-    getline(cin, Data.keys);
-    if (Data.keys[0] >= 'a' && Data.keys[0] <= 'z')
-        Data.keys[0] -= 32;
-    cout << "Enter the meaning of word: ";
-    getline(cin, Data.values);
-    addNode(t, createNode(Data));
-    cout << "Successfully addeData..." << endl;
-}
-
-void editMeaning(Tree t)
-{
-    string key;
-    Node *result;
-
-    cout << "Enter a word to edit meaning: ";
-    getline(cin, key);
-    if (key[0] >= 'a' && key[0] <= 'z')
-        key[0] -= 32;
-
-    result = find(t, key);
-    if (result == NULL)
-        cout << "Can't find the worData." << endl;
-    else
-    {
-        cout << result->Data.keys + ": " + result->Data.values << endl;
-        cout << "Enter new meaning for editing: ";
-        getline(cin, key);
-        result->Data.values = key;
-        cout << result->Data.keys + ": " + result->Data.values << endl;
-    }
-}
-
-void delAWord(Tree &t)
-{
-    string key;
-    Node *result;
-
-    cout << "Enter a word to delete: ";
-    getline(cin, key);
-    if (key[0] >= 'a' && key[0] <= 'z')
-        key[0] -= 32;
-
-    result = deleteNode(t, key);
-    if (result == NULL)
-        cout << "Can't find the worData." << endl;
-    else
-        cout << "Successfully deleteData..." << endl;
-}
-
-void outputTree(Tree t, fstream &fs1, fstream &fs2)
-{
-    if (t == NULL)
         return;
+    }
+    saveDataTree(root->left, fs1, fs2);
 
-    outputTree(t->left, fs1, fs2);
-    fs1 << t->Data.keys << endl;
-    fs2 << t->Data.keys + "  " + t->Data.values + "" << endl;
-    outputTree(t->right, fs1, fs2);
+    fs1 << root->data.keys << endl;
+    fs2 << root->data.values << endl;
+
+    saveDataTree(root->right, fs1, fs2);
 }
-// void save(Tree t)
+// void NLR(Tree root)
 // {
-//     fstream f;
-//     string fn;
-//     cout << "Enter file name to save: ";
-//     getline(cin, fn);
-//     f.open(fn + ".txt", ios::out);
-//     outputTree(t, f);
-//     f.close();
+//     if (root)
+//     {
+//         NLR(root->left);
+//         NLR(root->right);
+//     }
+// }
+// void LNR(Tree root)
+// {
+//     if (root)
+//     {
+//         LNR(root->left);
+// LNR(root->right);
+//     }
+// }
+// void LRN(Tree root)
+// {
+//     if (root)
+//     {
+//         LRN(root->left);
+//         LRN(root->right);
+//     }
 // }
 
-void menu(Tree t)
+void FindAndReplace(Tree &p, Tree &tree)
 {
-    load(t);
-    bool f = true;
-
-    while (f == true)
+    if (tree->left)
+        FindAndReplace(p, tree->left);
+    else
     {
-        cout << "1. Search" << endl;
-        cout << "2. Add" << endl;
-        cout << "3. Edit meaning" << endl;
-        cout << "4. Delete" << endl;
-        cout << "0. Exit" << endl;
-        cout << "Enter your choice: ";
+        p->data.keys = tree->data.keys;
+        p = tree;
+        tree = tree->right;
+    }
+}
 
+void DeleteNode(Tree &root, string x)
+{
+    if (root)
+    {
+        if (x > root->data.keys)
+        {
+            DeleteNode(root->right, x);
+        }
+        else if (x < root->data.keys)
+        {
+            DeleteNode(root->left, x);
+        }
+        else
+        {
+            Node *p = root;
+            if (!root->left)
+            {
+                root = root->right;
+            }
+            else if (!root->right)
+            {
+                root = root->left;
+            }
+            else
+            {
+                FindAndReplace(p, root->right);
+            }
+            delete p;
+        }
+    }
+    else
+    {
+        cout << endl
+             << "Not found!\n";
+    }
+}
+
+void processTree(Tree t, int seletc)
+{
+    string temps;
+    if (seletc == 1)
+    {
+        cout << endl
+             << "Input data to search: ";
+        cin >> temps;
+        Node *find = FindNode(t, temps);
+        if (find == NULL)
+        {
+            cout << endl
+                 << "Not find" << endl;
+        }
+        else
+        {
+            cout << endl
+                 << find->data.keys << " : " << find->data.values << endl;
+        }
+    }
+    else if (seletc == 2)
+    {
+        string keys;
+        string value;
+        cout << "Enter keys:";
+        cin.ignore();
+        cin >> keys;
+        cout << endl
+             << " Enter value: ";
+        cin.ignore();
+        getline(cin, value);
+        AddNode(t, CreateNode(keys, value));
+    }
+    else if (seletc == 3)
+    {
+        cout << endl
+             << "Enter keys delete ";
+        cin >> temps;
+        DeleteNode(t, temps);
+    }
+    else if (seletc == 4)
+    {
+        cout << endl
+             << "Input data to Edit: ";
+        cin >> temps;
+        Node *find = FindNode(t, temps);
+        if (find == NULL)
+        {
+            cout << endl
+                 << "Not find" << endl;
+        }
+        else
+        {
+            cout << endl
+                 << " prev data : " << find->data.keys << find->data.values << endl;
+            string values;
+            cout << "Enter new data ";
+            cin.ignore();
+            getline(cin, values);
+            find->data.values = values;
+        }
+    }
+}
+
+void selectionTree(Tree t)
+{
+    fstream fsTest("keys.txt", ios::in);
+    int size = sizeFileInput(fsTest);
+    info *arr = new info[size];
+    fsTest.close();
+    fstream fsKeys("keys.txt", ios::in);
+    fstream fsValue("values.txt", ios::in);
+    int count = 0;
+
+    while (!fsKeys.eof())
+    {
+        string keys;
+        string values;
+        getline(fsKeys, keys);
+        getline(fsValue, values);
+        arr[count].keys = keys;
+        arr[count].values = values;
+        count++;
+    }
+    for (int i = 0; i < count; i++)
+    {
+        AddNode(t, CreateNode(arr[i].keys, arr[i].values));
+    }
+    delete[] arr;
+    cout << endl
+         << count << endl;
+    fsKeys.close();
+    fsValue.close();
+
+    int seletc = 1;
+
+    while (seletc < 5)
+    {
+        cout << " AVL TREE " << endl;
+        cout << "1. SEARCH" << endl;
+        cout << "2. ADD DATA" << endl;
+        cout << "3. DELETE DATA" << endl;
+        cout << "4. EDIT DATA" << endl;
+        cout << "5. EXIT " << endl;
+        cout << " Choose your choice : ";
         int temp;
         cin >> temp;
-        cin.ignore();
-        cout << endl;
-
-        if (temp = 0)
-        {
-            f = false;
-        }
-
-        else if (temp = 1)
-        {
-            search(t);
-        }
-
-        else if (temp = 2)
-        {
-            addNewWord(t);
-        }
-
-        else if (temp = 3)
-        {
-            editMeaning(t);
-        }
-
-        else if (temp = 4)
-        {
-            delAWord(t);
-        }
+        seletc = temp;
+        processTree(t, seletc);
         system("pause");
     }
-
+    cout << "Save File" << endl;
     ofstream ofs1("keys.txt", std::ofstream::out);
     ofs1.close();
     ofstream ofs2("values.txt", std::ofstream::out);
     ofs2.close();
-    fstream fs1("keys.txt", ios::out, ios::app);
-    fstream fs2("values.txt", ios::out, ios::app);
-    outputTree(t, fs1, fs2);
+    fstream fs1("keys.txt", ios::out);
+    fstream fs2("values.txt", ios::out);
+    saveDataTree(t, fs1, fs2);
     fs1.close();
     fs2.close();
 }
+
 int main()
 {
-    //inputFile();
-    srand(time(NULL));
-    Tree t = NULL;
-    menu(t);
-
+    system("cls");
+    Tree t;
+    CreateTree(t);
+    cout << "Run" << endl;
+    fstream fsInFile("oxford English Dictionary.txt", ios::in);
+    input(fsInFile);
+    fsInFile.close();
+    selectionTree(t);
     return 0;
 }
